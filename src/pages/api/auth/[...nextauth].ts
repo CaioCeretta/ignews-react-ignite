059a429ce4667a181e9830c9d33ead4c2ce78503
Caio Ceretta <caioceretta@gmail.com>
2023-05-31 15:47:51 -0300

    Fixed some errors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,98 +1,76 @@
-import GitHubProvider from 'next-auth/providers/github'
-import NextAuth from "next-auth"
-import { query as q } from 'faunadb'
+import GitHubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth";
+import { query as q } from "faunadb";
 
-import { fauna } from '@/services/fauna'
+import { fauna } from "@/services/fauna";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET || '',
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || '',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-      
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async session({session}) {
-      
-    
+    async session({ session }) {
       try {
         const userActiveSubscription = await fauna.query(
           q.Get(
             q.Intersection([
               q.Match(
-                q.Index('subscription_by_user_ref'),
+                q.Index("subscription_by_user_ref"),
                 q.Select(
                   "ref",
                   q.Get(
                     q.Match(
-                      q.Index('user_by_email'),
-                      q.Casefold(session.user?.email || '')
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user?.email || "")
                     )
                   )
                 )
               ),
-              q.Match(
-                q.Index('subscription_by_status'),
-                "active"
-              )
+              q.Match(q.Index("subscription_by_status"), "active"),
             ])
           )
-        )
-
+        );
 
         return {
           ...session,
-          activeSubscription: userActiveSubscription
-        }
+          activeSubscription: userActiveSubscription,
+        };
       } catch {
-
         return {
           ...session,
           activeSubscription: null,
-        }
+        };
       }
     },
     async signIn({ user, account, profile }) {
-
-
-      const { email } = user
-
+      const { email } = user;
 
       try {
         await fauna.query(
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email || '')
-                )
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email || ""))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email: user.email } }
-            ),
+            q.Create(q.Collection("users"), { data: { email: user.email } }),
             q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email || '') 
-              )
+              q.Match(q.Index("user_by_email"), q.Casefold(user.email || ""))
             )
           )
-        )
-        return true
-      } catch(err){
-        console.log(err)
+        );
+        return true;
+      } catch (err) {
+        console.log(err);
         return false;
       }
-
-
     },
-  }
-})
+  },
+});
 
 // FaunaDB - HTTP: Utiliza um protocolo ao qual não se torna necessaria a conexão ativa a um banco de dados
 
