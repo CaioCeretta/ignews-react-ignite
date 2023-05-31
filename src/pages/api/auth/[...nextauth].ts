858594434e6506662b7aea75d46a3e74c3788c37
Caio Ceretta <caioceretta@gmail.com>
2023-05-31 17:13:40 -0300

    Fixed some errors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,7 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-      authorization: {
+       authorization: {
         params: {
           scope: 'read:user',
         },
@@ -58,10 +58,8 @@ export default NextAuth({
         await fauna.query(
           q.If(
             q.Not(
-              q.Exists(
                 q.Match(q.Index("user_by_email"), q.Casefold(user.email || ""))
-              )
-            ),
+              ),
             q.Create(q.Collection("users"), { data: { email: user.email } }),
             q.Get(
               q.Match(q.Index("user_by_email"), q.Casefold(user.email || ""))
